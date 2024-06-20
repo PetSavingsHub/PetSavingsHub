8db511df70743832280a42d25b1565ba5d9dbce6
JNLei <16848622+JNLei@users.noreply.github.com>
2024-06-19 21:48:29 -0400

    name size refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex w-full max-w-[1336px]">
       <div className="w-[50%] flex flex-col justify-center items-center gap-3">
         <Logo className="mr-10" />
-        <h1 className="text-[3vw] font-bold">
+        <h1 className="text-5xl  font-bold">
           PET SAVING HUB
         </h1>
       </div>
