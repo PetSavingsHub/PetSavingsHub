81a804367de36d1de7a39d4a3a784d677fa5498d
zhaoshihao1 <605097933zsh@gmail.com>
2024-06-28 16:40:22 -0400

    delete google ad files to chek error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import GoogleAdsense from "@/app/GoogleAdsense";
+//import GoogleAdsense from "@/app/GoogleAdsense";
 
 const inter = Inter({ subsets: ["latin"] });
 
